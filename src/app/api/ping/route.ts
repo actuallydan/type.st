92a094aa04d7cee1bf52ccaa12b260ea37e78adc
Dan Kral <dankral01@gmail.com>
2024-03-15 22:16:15 -0400

    allow formData post

---
+++ src/app/api/ping/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import redis from "@/utils/redis";
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+
+    if (!formData.get("c")) {
+      throw Error("you fucked up, son");
+    }
+
+    const id = Math.random().toString(16).replace(".", "");
+
+    const link = `https://type.st/p/${id}`;
+
+    const c = formData.get("c");
+
+    if (!c) {
+      console.error(formData);
+      throw Error(
+        "man I don't even know why this happened, check the logs homie"
+      );
+    }
+
+    let text = "";
+
+    if (typeof c === "string") {
+      text = c;
+    }
+
+    if (c instanceof File) {
+      const file: File = c as File;
+      text = await file.text();
+    }
+
+    await redis.set(id, text);
+
+    return new NextResponse(link);
+  } catch (err: any) {
+    console.error(err);
+    return new NextResponse(err.message, { status: 400 });
+  }
+}
