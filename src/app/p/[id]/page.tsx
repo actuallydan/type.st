27dacf81b2eacb2341d0e541125aaff81135f3ae
Dan Kral <dankral01@gmail.com>
2024-03-18 20:55:57 -0400

    change to monospace

--- src/app/p/[id]/page.tsx
+++ src/app/p/[id]/page.tsx
@@ -27,7 +27,7 @@ export default async function PastePage({
   return (
     <div className="min-w-[100dvw] min-h-[100dvh] bg-gray-100">
       <main className="p-4 py-8 w-full max-w-[1200px] m-auto h-[100dvh]">
-        <pre className="w-full h-full border-none font-sans text-gray-700 rounded-lg resize transition-all bg-white p-4 active:outline-none active:shadow-lg focus:shadow-lg focus-visible:outline-none">
+        <pre className="w-full h-full border-none font-mono text-gray-700 rounded-lg resize transition-all bg-white p-4 active:outline-none active:shadow-lg focus:shadow-lg focus-visible:outline-none">
           {data}
         </pre>
       </main>
