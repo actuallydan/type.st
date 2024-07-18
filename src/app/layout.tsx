4107aa66799b85ecc20d9bd0e3a3c0dd2391dcfd
Dan Kral <dankral01@gmail.com>
2024-07-18 12:08:49 -0400

    add github link to footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { GeistMono } from "geist/font/mono";
 import Link from "next/link";
 import "./globals.css";
 import { ReactQueryClientProvider } from "@/features/ReactQueryClientProvider";
-
 import History from "@/features/History";
 
 export const metadata: Metadata = {
@@ -23,11 +22,21 @@ export default function RootLayout({
         <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
           {children}
           <footer className="fixed bottom-0 flex items-center justify-end w-full py-2 px-4">
-            <Link href={"/"} className="mx-4 font-mono text-blue-500">
+            <Link href={"/"} className="mx-4 font-mono text-blue-500 text-base">
               home
             </Link>
-            <Link href={"/keys"} className=" font-mono text-blue-500">
+            <Link
+              href={"/keys"}
+              className="mx-4 font-mono text-blue-500 text-base"
+            >
               keys
+            </Link>
+            <Link
+              href={"https://github.com/actuallydan/type.st"}
+              className="mx-4 font-mono text-blue-500 text-base"
+              target="_blank"
+            >
+              github
             </Link>
           </footer>
           <History />
